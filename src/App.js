33ee96b0393b9a1670c8a71ea33e609c6cf99a2d
vikash <vikashc939@gmail.com>
2020-07-12 09:22:03 +0530

    added form input and custom button for reusability

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Homepage from "./containers/homepage/homepage";
 import ShopPage from "./containers/shopPage/shopPage";
 import Header from "./components/header/header";
+import signInAndSignUp from "./containers/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/sign-in" component={signInAndSignUp} />
       </Switch>
     </div>
   );

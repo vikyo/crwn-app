fd3a1dd798e1ce68d6eba6297c302bf97e057beb
vikash <vikashc939@gmail.com>
2020-07-15 10:38:04 +0530

    implemented redux-persist for cart state persistence

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/reducer";
 import cartReducer from "./cart/reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], // cart reducer state will be persisted, user is persisted in firebase , so no need to give here
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);

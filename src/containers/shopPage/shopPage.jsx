fa8a5672beb3a4aa6ea201425450cea6dea45b91
vikash <vikashc939@gmail.com>
2020-07-19 10:50:59 +0530

    implemented redux thunk, and fixed bug

--- src/containers/shopPage/shopPage.jsx
+++ src/containers/shopPage/shopPage.jsx
@@ -4,56 +4,46 @@ import { connect } from "react-redux";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
 import CollectionPage from "../collection/collectionPage";
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
-import { updateCollections } from "../../redux/shop/action";
+import { fetchCollectionsStartAsync } from "../../redux/shop/action";
 import WithSpinner from "../../components/withSpinner/withSpinner";
+import {
+  selectCollectionsisFetching,
+  selectIsCollectionsLoaded,
+} from "../../redux/shop/selector";
+import { createStructuredSelector } from "reselect";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  state = { loading: true };
-  unSubscribeFromSnapsot = null;
-  // Fetching the shop data
+  // Fetching the shop collection data
   componentDidMount() {
-    const { updateCollectionsFunction } = this.props;
-    const collectionRef = firestore.collection("collections");
-
-    // Using firebase REST api calls and fetch
-    // fetch(
-    //   "https://firestore.googleapis.com/v1/projects/crwn-db-ad650/databases/(default)/documents/collections"
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data));
-
-    collectionRef.get().then((snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collectionsMap);
-      updateCollectionsFunction(collectionsMap);
-      this.setState({ loading: false });
-    });
+    this.props.fecthCollections();
   }
 
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
     // console.log(CollectionsOverviewWithSpinner);
+    // console.log(isCollectionFetching);
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
           render={(props) => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionsOverviewWithSpinner
+              isLoading={!isCollectionsLoaded}
+              {...props}
+            />
           )}
         />
         <Route
           path={`${match.path}/:collectionCategory`}
           render={(props) => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner
+              isLoading={!isCollectionsLoaded}
+              {...props}
+            />
           )}
         />
       </div>
@@ -61,9 +51,15 @@ class ShopPage extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollectionsFunction: (collectionsObject) =>
-    dispatch(updateCollections(collectionsObject)),
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectCollectionsisFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded,
 });
 
-export default WithSpinner(connect(null, mapDispatchToProps)(ShopPage));
+const mapDispatchToProps = (dispatch) => ({
+  fecthCollections: () => dispatch(fetchCollectionsStartAsync()),
+});
+
+export default WithSpinner(
+  connect(mapStateToProps, mapDispatchToProps)(ShopPage)
+);
